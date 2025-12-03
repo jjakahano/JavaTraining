5652614ce74ad31abcd60c278bb2435f4fba3aac
jjakahano <jayfasd@gmail.com>
2025-12-03 17:10:36 +0800

    Push Activity1

---
+++ Activity1.java
@@ -0,0 +1,22 @@
+package com.bpi.test.main;
+import java.util.Scanner;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String name;
+		
+		System.out.print("What is your name? ");
+		
+		name = scanner.nextLine();
+		System.out.println("Hello " + name + "!");
+		
+		
+		
+		scanner.close();
+	}
+
+}
