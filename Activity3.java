0f4deb94242f94799be57397097cd39d697e306f
jjakahano <jayfasd@gmail.com>
2025-12-03 17:13:00 +0800

    Push Activity3

---
+++ Activity3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		/*
+		 * int age; System.out.print("Enter your age: "); age = scanner.nextInt();
+		 * 
+		 * double dage = age; System.out.println("Your age as int: " + age);
+		 * System.out.println("Your age as double: " + dage);
+		 */
+		
+		System.out.print("Enter first integer: ");
+		int first = scanner.nextInt();
+		
+		System.out.print("Enter second integer: ");
+		int second = scanner.nextInt();
+		
+		int sum = first + second;
+		int difference = first - second;
+		int product = first * second;
+		System.out.println("Sum: " + sum);
+		System.out.println("Difference: " + difference);
+		System.out.println("Product: " + product);
+		
+		
+		scanner.close();
+	}
+
+}
