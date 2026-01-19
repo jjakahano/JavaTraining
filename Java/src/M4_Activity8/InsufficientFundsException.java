66ad1d0031455b00507fa094e934376022f42fc1
ddddjkkkk <jjaykahano@gmail.com>
2026-01-20 01:26:31 +0800

    Initial commit for M4_Activity8

---
+++ Java/src/M4_Activity8/InsufficientFundsException.java
@@ -0,0 +1,23 @@
+package M4_Activity8;
+
+public class InsufficientFundsException extends Exception{
+    private double balance;
+    private double requestedAmount;
+
+    public InsufficientFundsException(String message, double balance, double requestedAmount){
+
+        super(message);
+        this.balance = balance;
+        this.requestedAmount = requestedAmount;
+    }
+
+    public double getBalance(double balance){
+        return balance;
+    }
+
+    public double getRequestedAmount(double requestedAmount){
+        return requestedAmount;
+    }
+
+
+}
---
+++ Java/src/M4_Activity8/LoggingExample.java
@@ -0,0 +1,57 @@
+package M4_Activity8;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingExample {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingExample.class);
+
+    private double balance = 10000;
+
+    public void deposit(double amount){
+        //Checking amount
+
+        if (amount <= 0){
+            logger.error("You are not depositing a valid amount: {}, should be greater than Zero", amount);
+            return;
+        }
+
+        balance += amount;
+        logger.info("Successfully deposited: P{}, New balance: P{}", amount, balance);
+    }
+
+    public void withdraw(double amount){
+        logger.info("Withdrawal request: P{}", amount);
+
+        try {
+            // Check balance
+            if (amount > balance){
+                logger.warn("You are withdrawing more than your current balance! Balance = P{}", balance);
+            }
+            // Process withdrawal
+            balance -= amount;
+            // Log success withdrawal
+            logger.info("Successfully withdrawn: P{}", amount);
+
+        }catch (Exception e){
+            logger.error("Withdrawal failed: {}", e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+//        logger.error("ERROR: Something is broken! (Critical)");
+//        logger.warn("WARN: This looks suspicious... (Warning)");
+//        logger.info("INFO: The application is doing its job. (Normal)");
+//        logger.debug("DEBUG: Database query returned 5 rows. (Developer Info)");
+//        logger.trace("TRACE: Entering method calculateInterest(). (Very Detailed)");
+
+        LoggingExample bankAccount = new LoggingExample();
+        bankAccount.withdraw(3000);
+        bankAccount.withdraw(5000);
+        bankAccount.withdraw(4000);
+        bankAccount.deposit(-2000);
+        bankAccount.deposit(5000);
+
+    }
+
+}
